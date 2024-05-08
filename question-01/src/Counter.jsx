6567d5f1863ca78b7bb59d785850e776feb427de
Yash-Vachhani <jigarmakwana34065@gmail.com>
2024-05-08 18:31:32 +0530

    pr2

---
+++ question-01/src/Counter.jsx
@@ -0,0 +1,52 @@
+
+
+import { Component } from "react";
+class Counter extends Component {
+    constructor() {
+        super();
+        this.state = {
+            count: 0
+        }
+    }
+    Increment() {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+    Dicrement() {
+        this.setState({
+            count: this.state.count - 1
+        })
+    }
+    Reset() {
+        this.setState({
+            count: 0
+        })
+    }
+
+    render() {
+        return (
+            <div align="center" className="Counting" >
+                <h1>Counter App</h1>
+              
+                <button className="plus" onClick={() => this.Increment()} >+</button>
+                <span>Count :- {this.state.count}</span>
+                <button className="minus" onClick={() => this.Dicrement()}>-</button>
+                <br></br>
+                <button className="zero"  onClick={() => this.Reset()}>Reset</button>
+
+
+            </div>
+
+        )
+    }
+}
+export default Counter;
+
+
+
+
+
+
+
+
